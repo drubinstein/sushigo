32c0310f27236a6c7eb1ccc631e618fab82f478c
David Rubinstein <drubinstein91@gmail.com>
2016-10-22 18:35:42 -0400

    Completed cmdline version of the game.

--- Cards.py
+++ Cards.py
@@ -1,32 +1,32 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 class Card(metaclass=ABCMeta):
     @abstractmethod
-    def get_name():
+    def get_name(self):
         pass
 
 class Tempura(Card):
-    def get_name():
+    def get_name(self):
         return "Tempura"
 
 class Sashimi(Card):
-    def get_name():
+    def get_name(self):
         return "Sashimi"
 
 class Pudding(Card):
-    def get_name():
+    def get_name(self):
         return "Pudding"
 
 class Wasabi(Card):
-    def get_name():
+    def get_name(self):
         return "Wasabi"
 
 class Chopsticks(Card):
-    def get_name():
+    def get_name(self):
         return "Chopsticks"
 
 class Dumpling(Card):
-    def get_name():
+    def get_name(self):
         return "Dumpling"
 
 class Maki(Card):
@@ -36,31 +36,30 @@ class Maki(Card):
     def get_maki(self):
         return self.n_maki
 
-    def get_name():
-        return "Maki {0}".format(self.n_maki)
+    def get_name(self):
+        return "Maki-{0}".format(self.n_maki)
 
 class Nigiri(Card):
-    @abstractmethod
-    def __init__():
+    def __init__(self):
         self.wasabi = False
 
 class Squid(Nigiri):
     def get_value(self):
         return 9 if wasabi else 3
 
-    def get_name():
+    def get_name(self):
         return "Squid Nigiri"
 
 class Salmon(Nigiri):
     def get_value(self):
         return 6 if wasabi else 2
 
-    def get_name():
+    def get_name(self):
         return "Salmon Nigiri"
 
 class Egg(Nigiri):
     def get_value(self):
         return 3 if wasabi else 1
 
-    def get_name():
+    def get_name(self):
         return "Egg Nigiri"
--- sushigo.py
+++ sushigo.py
@@ -3,10 +3,13 @@ import random
 
 class SushiGo:
     class Player:
+        hand = []
+        plate = []
+        score = 0
         def __init__(self):
             self.score = 0
             self.hand = []
-            self.pallete = []
+            self.plate = []
             self.n_tempura = 0
             self.n_sashimi = 0
             self.n_pudding = 0
@@ -14,34 +17,41 @@ class SushiGo:
             self.n_dumplings = 0
             self.wasabi_unused = 0
 
-        def get_hand_size(self):
-            return len(self.hand)
+        def get_hand_str(self):
+            s = ""
+            for c in self.hand:
+                s += c.get_name() + " "
+            return s
 
-        #move card index h in hand (must be range 0:len(hand))
-        #update score as well
-        def mov_hand_pallete(self, h=0):
-            if type(hand[h]) is Sashimi:
+        def get_plate_str(self):
+            s = ""
+            for c in self.plate:
+                s += c.get_name() + " "
+            return s
+
+        def mov_hand_plate(self, h=0):
+            if type(self.hand[h]) is Sashimi:
                 self.n_sashimi += 1
-            if type(hand[h]) is Tempura:
+            if type(self.hand[h]) is Tempura:
                 self.n_tempura += 1
-            if type(hand[h]) is Pudding:
+            if type(self.hand[h]) is Pudding:
                 self.n_pudding += 1
-            if type(hand[h]) is Maki:
-                self.n_maki += hand[h].get_maki()
-            if type(hand[h]) is Dumpling:
+            if type(self.hand[h]) is Maki:
+                self.n_maki += self.hand[h].get_maki()
+            if type(self.hand[h]) is Dumpling:
                 self.n_dumplings += 1
-            if type(hand[h]) is Wasabi:
+            if type(self.hand[h]) is Wasabi:
                 self.wasabi_unused += 1
-            if issubclass(hand[h], Nigiri) and self.wasabi_unused > 0
-                hand[h].wasabi = True
+            if issubclass(self.hand[h], Nigiri) and self.wasabi_unused > 0:
+                self.hand[h].wasabi = True
                 self.wasabi_unused -= 1
 
-            self.pallete.append(hand[h])
-            del hand[h]
+            self.plate.append(self.hand[h])
+            del self.hand[h]
 
         def reset(self):
             self.hand = []
-            self.pallete = []
+            self.plate = []
             self.n_tempura = 0
             self.n_sashimi = 0
             self.n_pudding = 0
@@ -50,23 +60,20 @@ class SushiGo:
             self.wasabi_unused = 0
 
     def __init__(self,n_players):
-        #Cycle direction
-        self.cycle = True
-
-        deck = [Tempura    for _ in range(1,15) \
-               ,Sashimi    for _ in range(1,14) \
-               ,Dumpling   for _ in range(1,14) \
-               ,Maki(2)    for _ in range(1,12) \
-               ,Maki(3)    for _ in range(1,8)  \
-               ,Maki(1)    for _ in range(1,6)  \
-               ,Salmon     for _ in range(1,10) \
-               ,Squid      for _ in range(1,5)  \
-               ,Egg        for _ in range(1,5)  \
-               ,Pudding    for _ in range(1,10) \
-               ,Wasabi     for _ in range(1,6)  \
-               ,Chopsticks for _ in range(1,4)]
+        self.deck = [Tempura() for _ in range(0,15)] \
+                  + [Sashimi() for _ in range(0,14)] \
+                  + [Dumpling() for _ in range(0,14)] \
+                  + [Maki(2) for _ in range(0,12)] \
+                  + [Maki(1) for _ in range(0,6)] \
+                  + [Maki(3) for _ in range(0,8)] \
+                  + [Salmon() for _ in range(0,10)] \
+                  + [Squid() for _ in range(0,5)] \
+                  + [Egg() for _ in range(0,5)] \
+                  + [Pudding() for _ in range(0,10)] \
+                  + [Wasabi() for _ in range(0,6)] \
+                  + [Chopsticks() for _ in range(0,4)]
         self.n_players = n_players
-        self.players = [Player for _ in range(1,n_players)]
+        self.players = [SushiGo.Player() for _ in range(0,n_players)]
 
         if self.n_players == 2: self.start_hand_size = 10
         if self.n_players == 3: self.start_hand_size = 9
@@ -76,13 +83,13 @@ class SushiGo:
         #shuffle the master deck for the game
         #initialize turn
         self.round = 0
-        random.shuffle(master_deck)
+        random.shuffle(self.deck)
 
     def begin_turn(self):
         #hand out cards to each players
         for p in self.players:
-            p.hand.append(deck[0:self.start_hand_size])
-            del deck[0:self.start_hand_size]
+            p.hand = self.deck[0:self.start_hand_size]
+            del self.deck[0:self.start_hand_size]
 
     def get_max_pudding(self):
         max_pudding = 0
@@ -104,90 +111,110 @@ class SushiGo:
     def score_players(self):
         scores = [0]*self.n_players
         max_pudding = self.get_max_pudding()
-        max_makis = self.get_max_makis()
-        for p in range(0,self.n_players-1):
-            scores[p] = self.players[p].n_tempura/2*5 \
-                      + self.players[p].n_sashimi/3*10 \
+        max_makis = self.get_top_makis()
+
+        for p in range(0,self.n_players):
+            scores[p] = int(self.players[p].n_tempura/2)*5 \
+                      + int(self.players[p].n_sashimi/3)*10
             if max_pudding > 0:
                 if self.players[p].n_pudding == max_pudding:
                     scores[p] += 6
-                else
+                else:
                     scores[p] -= 6
 
-            if max_makis[0] > 0:
-                if self.players[p].n_maki == max_maki[0]:
+            if max_makis[0] > 0 and self.players[p].n_maki == max_makis[0]:
                     scores[p] += 6
-                elif self.players[p].n_maki == max_maki[1]:
+            elif max_makis[1] > 0 and self.players[p].n_maki == max_makis[1]:
+                    scores[p] += 3
 
-            if self.players[p].n_dumplings == 1: scores[p] += 1
-            if self.players[p].n_dumplings == 2: scores[p] += 3
-            if self.players[p].n_dumplings == 3: scores[p] += 6
-            if self.players[p].n_dumplings == 4: scores[p] += 10
-            if self.players[p].n_dumplings == 5: scores[p] += 15
+            if self.players[p].n_dumplings == 1:
+                scores[p] += 1
+            if self.players[p].n_dumplings == 2:
+                scores[p] += 3
+            if self.players[p].n_dumplings == 3:
+                scores[p] += 6
+            if self.players[p].n_dumplings == 4:
+                scores[p] += 10
+            if self.players[p].n_dumplings == 5:
+                scores[p] += 15
 
-            #now just loop through the pallete to deal with the nigiri
+            #now just loop through the plate to deal with the nigiri
             for card in self.players[p].hand:
                 if issubclass(card,Nigiri):
                     scores[p] += card.get_value()
 
-    def play_round(self,cycle ):
-        for c in range(0,self.start_hand_size-1):
+        return scores
+
+    def play_round(self, cycle):
+        for c in range(0,self.start_hand_size):
+            print("PASS {0}".format(c))
             for p in self.players:
-                print("Hand: {0}\nPlate: {1}".format(p.hand, p.pallete)
-                h = input("Please enter the index of the card you want to play."
-                          " Must be between 0 and {0}. If you have and want to use"
-                          " chopsticks from your pallete enter c[0-9][0-9] where"
-                          "the numbers represent the cards from the hand you want to use".format(len(p.hand)-1))
+                print("Hand: {0}\nPlate: {1}".format(p.get_hand_str(), p.get_plate_str()))
                 while(True):
+                    if len(p.hand) == 1:
+                        p.mov_hand_plate(0)
+                        break
+
+                    h = input("Please enter the index of the card you want to play."
+                          " Must be between 0 and {0}. If you have and want to use"
+                          " chopsticks from your plate enter c[0-9][0-9] where"
+                          "the numbers represent the cards from the hand you want to use\n".format(len(p.hand)-1))
+
                     if len(h) == 0:
                         print("You have not entered anything...")
                         continue
-                    if h == "c" and p.hand.count(Chopsticks) > 0
+                    if h == "c" and p.hand.count(Chopsticks) > 0:
                         if h[1].isdigit() and h[2].isdigit():
                             h1 = int(h[1])
                             h2 = int(h[2])
-                            if h1 < len(p.hand) or h1 < len(p.hand):
+                            if h1 >= len(p.hand) or h1 >= len(p.hand):
                                 print("You have entered an invalid hand index")
                                 continue
 
-                            p.pallete.remove(Chopsticks)
-                            p.mov_hand_pallete(h1)
-                            p.mov_hand_pallete(h2)
-                    if h.isdigit():
-                        h = int(h)
-                        if h < len(p.hand):
-                            print("You have entered an invalid hand index")
+                            p.plate.remove(Chopsticks)
+                            p.mov_hand_plate(h1)
+                            p.mov_hand_plate(h2)
+                            print("SUSHI GO!")
+                            break
+                    if h[0].isdigit():
+                        idx = int(h[0])
+                        if idx >= len(p.hand):
+                            print("You have entered an invalid hand index: {0}".format(idx))
                             continue
-                        p.mov_hand_pallete(h)
-                    break
+                        p.mov_hand_plate(idx)
+                        break
             #Now cycle the hands
 
-            if cycle == RIGHT:
+            if cycle:
                 for p in range(0,self.n_players):
-                    self.players[p].hand, self.players[(p+1)%self.n_players] =  \
-                    self.players[(p+1)%self.n_players].hand, self.players[p]
-            elif cycle == LEFT:
+                    self.players[p].hand, self.players[(p+1)%self.n_players].hand =  \
+                    self.players[(p+1)%self.n_players].hand, self.players[p].hand
+            else:
                 for p in range(self.n_players-1,-1,-1):
-                    self.players[p].hand, self.players[p-1] =  \
-                    self.players[p-1].hand, self.players[p]
+                    self.players[p].hand, self.players[p-1].hand =  \
+                    self.players[p-1].hand, self.players[p].hand
 
     def game(self):
+        print("WELCOME TO SUSHI GO!")
         #3 rounds per game
+        cycle = True
         for r in range(0,3):
+            self.begin_turn()
             self.play_round(cycle)
             cycle = not cycle
             scores = self.score_players()
+            print("Scores for the round: ")
+            print(scores)
             #record scores and reset for next turn
-            for p in range(0,self.n_players-1):
-                self.begin_turn()
+            for p in range(0,self.n_players):
                 self.players[p].score += scores[p]
-                p.reset()
+                self.players[p].reset()
 
+            print("Score Totals:\n")
+            for n in range(0,self.n_players):
+                print("\tPlayer {0}: {1}".format(n,self.players[n].score))
 
-        print("Game over\nScores:\n")
-        for n in range(0,self.n_players-1):
-            print("\tPlayer {0}: {1}".format(n,self.players[n].score))
-
+        print("Game over\n")
 
 game = SushiGo(3)
 game.game()
